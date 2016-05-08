4656774b902d2dcfe629f18faa2d732d61d77973
udhavsethi <es12b1022@iith.ac.in>
2016-05-09 00:21:44 +0530

    Modularized all actor scripts

--- hete_sim_search/web/scripts/AMA.py
+++ hete_sim_search/web/scripts/AMA.py
@@ -2,22 +2,22 @@ from functions import *
 
 if __name__ == '__main__':
 
-	actor_input = str(sys.argv[1])
+	query = str(sys.argv[1])
 	top_K = int(sys.argv[2])
 	
 	#xx
-	query_xx = "MATCH (a:actor {name:'"+str(actor_input)+"'})-[ACTED_IN]->(m) \
+	query_xx = "MATCH (a:actor {name:'"+query+"'})-[ACTED_IN]->(m) \
 				MATCH (m)<-[r:ACTED_IN]-(a) \
 				RETURN a as actor,count(a.name) as count"
 	
 	#xy
-	query_xy = "MATCH (a:actor {name:'"+str(actor_input)+"'})-[r1:ACTED_IN]->(m1:movie)<-[r2:ACTED_IN]-(b:actor) \
+	query_xy = "MATCH (a:actor {name:'"+query+"'})-[r1:ACTED_IN]->(m1:movie)<-[r2:ACTED_IN]-(b:actor) \
 				RETURN b as actor,count(b.name) as count"
 	
 	#yy
-	query_yy = "MATCH (a:actor {name:'"+str(actor_input)+"'})-[r1:ACTED_IN]->(m1:movie)<-[r2:ACTED_IN]-(b:actor) \
+	query_yy = "MATCH (a:actor {name:'"+query+"'})-[r1:ACTED_IN]->(m1:movie)<-[r2:ACTED_IN]-(b:actor) \
 				MATCH (b)-[r3:ACTED_IN]->(m) \
 				MATCH (m)<-[r4:ACTED_IN]-(b) \
 				RETURN b as actor,count(b.name) as count"
 	
-	find_topK_results(actor_input, top_K, query_xx, query_xy, query_yy)
+	find_topK_results(query, top_K, query_xx, query_xy, query_yy)
